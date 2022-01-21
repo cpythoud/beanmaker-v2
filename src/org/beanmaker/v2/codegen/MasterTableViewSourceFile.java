48bdf9d2235a98bb666a25b152f3e6c7b645f5bd
chris <git@pythoud.net>
2022-01-21 11:31:48 -0500

    Create code for MasterTableView.java

---
+++ src/org/beanmaker/v2/codegen/MasterTableViewSourceFile.java
@@ -0,0 +1,38 @@
+package org.beanmaker.v2.codegen;
+
+import org.jcodegen.java.FunctionArgument;
+import org.jcodegen.java.FunctionCall;
+
+public class MasterTableViewSourceFile extends BaseCode {
+
+    public MasterTableViewSourceFile(String packageName) {
+        super("MasterTableView", packageName);
+
+        createSourceCode();
+    }
+
+    @Override
+    protected void addImports() {
+        importsManager.addImport("org.beanmaker.v2.runtime.BaseMasterTableView");
+        importsManager.addImport("org.beanmaker.v2.runtime.DbBeanLocalization");
+    }
+
+    @Override
+    protected void decorateJavaClass() {
+        javaClass.markAsAbstract().extendsClass("BaseMasterTableView");
+    }
+
+    @Override
+    protected void addConstructors() {
+        javaClass
+                .addContent(javaClass.createConstructor()
+                        .addArgument(new FunctionArgument("String", "tableId"))
+                        .addArgument(new FunctionArgument("DbBeanLocalization", "localization"))
+                        .addContent(new FunctionCall("super").byItself().addArguments("tableId", "localization")))
+                .addContent(EMPTY_LINE);
+    }
+
+    @Override
+    protected void addCoreFunctionality() { }
+
+}
