8259997452658139fcc3525405e4a3edd3fe107f
chris <git@pythoud.net>
2022-01-23 16:45:41 -0500

    Create code for BeanParameters.java

---
+++ src/org/beanmaker/v2/codegen/BeanParametersSourceFile.java
@@ -0,0 +1,39 @@
+package org.beanmaker.v2.codegen;
+
+import org.jcodegen.java.FunctionCall;
+import org.jcodegen.java.VarDeclaration;
+import org.jcodegen.java.Visibility;
+
+public class BeanParametersSourceFile extends BeanCode {
+
+    public BeanParametersSourceFile(String beanName, String packageName) {
+        super(beanName, packageName, "Parameters");
+
+        createSourceCode();
+    }
+
+    @Override
+    protected void decorateJavaClass() {
+        javaClass.extendsClass(beanName  + "ParametersBase");
+    }
+
+    @Override
+    protected void addStaticProperties() {
+        javaClass
+                .addContent(VarDeclaration.declareAndInit(className, "INSTANCE").markAsStatic().markAsFinal())
+                .addContent(EMPTY_LINE);
+    }
+
+    @Override
+    protected void addConstructors() {
+        javaClass
+                .addContent(javaClass.createConstructor()
+                        .visibility(Visibility.PRIVATE)
+                        .addContent(new FunctionCall("super").byItself()))
+                .addContent(EMPTY_LINE);
+    }
+
+    @Override
+    protected void addCoreFunctionality() { }
+
+}
