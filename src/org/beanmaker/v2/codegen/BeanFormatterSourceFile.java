d7a93af9a715a85f0beae3eaff25e35a19f4a224
chris <git@pythoud.net>
2022-01-24 12:34:59 -0500

    Create code for BeanFormatter.java

---
+++ src/org/beanmaker/v2/codegen/BeanFormatterSourceFile.java
@@ -0,0 +1,39 @@
+package org.beanmaker.v2.codegen;
+
+import org.jcodegen.java.FunctionCall;
+import org.jcodegen.java.VarDeclaration;
+import org.jcodegen.java.Visibility;
+
+public class BeanFormatterSourceFile extends BeanCode {
+
+    public BeanFormatterSourceFile(String beanName, String packageName) {
+        super(beanName, packageName, "Formatter");
+
+        createSourceCode();
+    }
+
+    @Override
+    protected void decorateJavaClass() {
+        javaClass.extendsClass(beanName  + "FormatterBase");
+    }
+
+    @Override
+    protected void addStaticProperties() {
+        javaClass
+                .addContent(VarDeclaration.declareAndInit(className, "INSTANCE").markAsStatic().markAsFinal())
+                .addContent(EMPTY_LINE);
+    }
+
+    @Override
+    protected void addConstructors() {
+        javaClass
+                .addContent(javaClass.createConstructor()
+                        .visibility(Visibility.PRIVATE)
+                        .addContent(new FunctionCall("super").byItself()))
+                .addContent(EMPTY_LINE);
+    }
+
+    @Override
+    protected void addCoreFunctionality() { }
+
+}
