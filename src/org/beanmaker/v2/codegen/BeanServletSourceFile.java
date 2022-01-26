b37b15a658198746a2d1bd075f36351830a60069
chris <git@pythoud.net>
2022-01-26 12:41:16 -0500

    Create code for BeanServlet.java

---
+++ src/org/beanmaker/v2/codegen/BeanServletSourceFile.java
@@ -0,0 +1,21 @@
+package org.beanmaker.v2.codegen;
+
+import org.jcodegen.java.Visibility;
+
+public class BeanServletSourceFile extends BeanCode {
+
+    public BeanServletSourceFile(String beanName, String packageName) {
+        super(beanName, packageName, null, "Servlet");
+
+        createSourceCode();
+    }
+
+    @Override
+    protected void decorateJavaClass() {
+        javaClass.visibility(Visibility.PUBLIC).extendsClass(beanName + "ServletBase");
+    }
+
+    @Override
+    protected void addCoreFunctionality() { }
+
+}
