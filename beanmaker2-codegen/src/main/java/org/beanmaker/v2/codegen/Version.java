6a29d1d10d1edbbbf2ca23c6678f517ca1adc745
chris <git@pythoud.net>
2022-03-22 17:21:28 -0500

    Add build number to library information for development versions

--- beanmaker2-codegen/src/main/java/org/beanmaker/v2/codegen/Version.java
+++ beanmaker2-codegen/src/main/java/org/beanmaker/v2/codegen/Version.java
@@ -1,17 +1,20 @@
 package org.beanmaker.v2.codegen;
 
+import org.beanmaker.v2.util.Strings;
+
 /**
  * This class gives information about the VERSION of this library.
  */
 public class Version {
 
     private static final String VERSION = "1.0-SNAPSHOT";
+    private static final String VERSION_BUILD = "20322";
 
     /**
      * @return the version number of this library
      */
     public static String get() {
-        return VERSION;
+        return VERSION + (Strings.isEmpty(VERSION_BUILD) ? "" : "-build#" + VERSION_BUILD);
     }
 
 }
