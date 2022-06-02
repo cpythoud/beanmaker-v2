4b9caf8be1803b02e19fe3fec26e5df0c9ebfc44
chris <git@pythoud.net>
2022-06-02 13:39:38 -0500

    Add utility class for converting exception stack traces to strings

---
+++ beanmaker2-util/src/main/java/org/beanmaker/v2/util/Exceptions.java
@@ -0,0 +1,23 @@
+package org.beanmaker.v2.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * This class contains methods related to the management of java Exception.
+ */
+public class Exceptions {
+
+    /**
+     * Take any exception and returns its stack trace as a thing
+     * @param throwable, the run time or checked exception which stack trace we are interested in
+     * @return the throwable stack trace as a String
+     */
+    public static String getStackTrace(Throwable throwable) {
+        var stringWriter = new StringWriter();
+        var printWriter = new PrintWriter(stringWriter);
+        throwable.printStackTrace(printWriter);
+        return stringWriter.toString();
+    }
+
+}
