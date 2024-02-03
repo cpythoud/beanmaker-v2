af308c659e7a3a4fa93fa04027ae133fa6885472
chris <git@pythoud.net>
2024-02-03 13:44:01 -0500

    Update beanmaker.file-upload.js version and functionality
    Updated the version number and functioning of beanmaker.file-upload.js script within beanmaker2 runtime. Added a new object (BEANMAKER_FILE_UPLOAD_OPTIONS) that allows custom file upload check functions on a per-form, per-field basis. Further, modified 'change' event handler for file inputs to retrieve custom check functions, if provided, for the current form and field.

--- beanmaker2-runtime/src/main/web/js/beanmaker.file-upload.js
+++ beanmaker2-runtime/src/main/web/js/beanmaker.file-upload.js
@@ -1,15 +1,35 @@
-// beanmaker.file-upload.js -- v0.1.1 -- 2024-01-31
+// beanmaker.file-upload.js -- v0.2 -- 2024-02-03
+
+BEANMAKER_FILE_UPLOAD_OPTIONS = {
+    // * add entry: BEANMAKER_FILE_UPLOAD_OPTIONS.forms.formName = { fieldName1: function1() { ... }, fieldName2: function2() { ... }, ... }
+    forms: { },
+
+    getFunction: function (form, field) {
+        if (this.forms[form] !== undefined) {
+            const checkFunction = this.forms[form][field];
+            if (checkFunction !== undefined && typeof checkFunction === 'function') {
+                return checkFunction;
+            }
+        }
+        return undefined;
+    }
+};
 
 $(document).ready(function() {
 
     const $body = $('body');
 
     $body.on('change', 'input.file', function () {
-        const id = $(this).attr('id');
-        $('#display_' + id)
-            .text($(this).val().replace(/C:\\fakepath\\/i, ''));
-        $('#remove_' + id).removeClass('hidden');
-        $('#delete_' + id).remove();
+        const $this = $(this);
+        const formName = $this.closest('form').attr('name');
+        const checkFunction = BEANMAKER_FILE_UPLOAD_OPTIONS.getFunction(formName, $this.attr('name'));
+        if (checkFunction === undefined || checkFunction()) {
+            const id = $(this).attr('id');
+            $('#display_' + id)
+                .text($(this).val().replace(/C:\\fakepath\\/i, ''));
+            $('#remove_' + id).removeClass('hidden');
+            $('#delete_' + id).remove();
+        }
     });
 
     $body.on('click', '.remove-file', function () {
