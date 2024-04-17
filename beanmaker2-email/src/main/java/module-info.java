7fef146c5c581b3cca0349c2ba44e5457ef3afe3
chris <git@pythoud.net>
2024-04-17 09:38:56 -0500

    Add mimeType field to FileAttachment, with automatic detection if unspecified
    The mimeType can be specified or detected. The detection code use the Tika library and dependencies have been updated accordingly.

--- beanmaker2-email/src/main/java/module-info.java
+++ beanmaker2-email/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 @SuppressWarnings("module")
 module beanmaker.v2.email {
     requires org.beanmaker.v2.util;
+    requires org.apache.tika.core;
 
     exports org.beanmaker.v2.email;
 }
--- beanmaker2-email/src/main/java/org/beanmaker/v2/email/FileAttachment.java
+++ beanmaker2-email/src/main/java/org/beanmaker/v2/email/FileAttachment.java
@@ -1,40 +1,35 @@
 package org.beanmaker.v2.email;
 
+import org.apache.tika.Tika;
+
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class FileAttachment {
 
+    private static final Tika TIKA = new Tika();
+
     private final Path filePath;
     private final String fileName;
+    private final String mimeType;
 
-    private FileAttachment(Path filePath, String fileName) {
+    private FileAttachment(Path filePath, String fileName, String mimeType) {
         this.filePath = filePath;
         this.fileName = fileName;
+        this.mimeType = mimeType;
     }
 
-    public static FileAttachment create(Path filePath, String fileName) {
-        return new FileAttachment(filePath, fileName);
+    public static Builder builder() {
+        return new Builder();
     }
 
-    public static FileAttachment create(File file, String fileName) {
-        return create(file.toPath(), fileName);
-    }
-
-    public static FileAttachment create(String filePath, String fileName) {
-        return create(Path.of(filePath), fileName);
-    }
-
-    public static FileAttachment create(Path filePath) {
-        return new FileAttachment(filePath, filePath.getFileName().toString());
-    }
-
-    public static FileAttachment create(File file) {
-        return create(file.toPath());
-    }
-
-    public static FileAttachment create(String filePath) {
-        return create(Path.of(filePath));
+    public static String detectMimeType(File file) {
+        try {
+            return TIKA.detect(file);
+        } catch (IOException ioex) {
+            throw new RuntimeException(ioex);
+        }
     }
 
     public Path getFilePath() {
@@ -43,6 +38,57 @@ public class FileAttachment {
 
     public String getFileName() {
         return fileName;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public static class Builder {
+        private Path filePath;
+        private String fileName;
+        private String mimeType;
+
+        private Builder() { }
+
+        public Builder filePath(Path filePath) {
+            this.filePath = filePath;
+            return this;
+        }
+
+        public Builder file(File file) {
+            this.filePath = file.toPath();
+            return this;
+        }
+
+        public Builder filePath(String filePath) {
+            this.filePath = Path.of(filePath);
+            return this;
+        }
+
+        public Builder fileName(String fileName) {
+            this.fileName = fileName;
+            return this;
+        }
+
+        public Builder mimeType(String mimeType) {
+            this.mimeType = mimeType;
+            return this;
+        }
+
+        public FileAttachment build() {
+            if (filePath == null) {
+                throw new IllegalStateException("File path cannot be null");
+            }
+            if (fileName == null) {
+                fileName = filePath.getFileName().toString();
+            }
+            if (mimeType == null) {
+                mimeType = detectMimeType(filePath.toFile());
+            }
+
+            return new FileAttachment(filePath, fileName, mimeType);
+        }
     }
 
 }
