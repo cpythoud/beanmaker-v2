df050002c208d8d29a304bda15a64c3127bd6796
chris <git@pythoud.net>
2024-12-24 11:28:44 -0500

    Refactor `Names` class to use modern Java syntax.
    Replaced explicit type declarations with `var` for improved readability and conciseness. Simplified sorting logic by replacing anonymous classes with lambda expressions or method references where applicable. These changes make the codebase more maintainable and aligned with modern Java practices.

--- beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/Names.java
+++ beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/Names.java
@@ -12,7 +12,7 @@ import java.util.Locale;
 public class Names {
 
     public static <B extends NamedDbBean> List<String> getList(Collection<B> beans) {
-        List<String> names = new ArrayList<String>();
+        var names = new ArrayList<String>();
 
         for (B bean: beans)
             names.add(bean.getDisplayName());
@@ -21,36 +21,26 @@ public class Names {
     }
 
     public static <B extends NamedDbBean> List<String> getSortedList(Collection<B> beans) {
-        List<String> names = getList(beans);
+        var names = getList(beans);
         Collections.sort(names);
         return names;
     }
 
     public static <B extends NamedDbBean> void sortList(List<B> beans) {
-        Collections.sort(beans, new Comparator<B>() {
-            @Override
-            public int compare(B bean1, B bean2) {
-                return bean1.getDisplayName().compareTo(bean2.getDisplayName());
-            }
-        });
+        beans.sort(Comparator.comparing(NamedDbBean::getDisplayName));
     }
 
     public static <B extends NamedDbBean> void sortList(List<B> beans, Locale locale) {
-        Collator collator = Collator.getInstance(locale);
+        var collator = Collator.getInstance(locale);
         collator.setStrength(Collator.PRIMARY);
-        Collections.sort(beans, new Comparator<B>() {
-            @Override
-            public int compare(B bean1, B bean2) {
-                return collator.compare(bean1.getDisplayName(), bean2.getDisplayName());
-            }
-        });
+        beans.sort((bean1, bean2) -> collator.compare(bean1.getDisplayName(), bean2.getDisplayName()));
     }
 
     public static <B extends MultiLingualNamedDbBean> List<String> getList(
             Collection<B> beans,
             DbBeanLanguage dbBeanLanguage)
     {
-        List<String> names = new ArrayList<String>();
+        var names = new ArrayList<String>();
 
         for (B bean: beans)
             names.add(bean.getDisplayName(dbBeanLanguage));
@@ -71,21 +61,17 @@ public class Names {
             List<B> beans,
             DbBeanLanguage dbBeanLanguage)
     {
-        Collator collator = Collator.getInstance(dbBeanLanguage.getLocale());
+        var collator = Collator.getInstance(dbBeanLanguage.getLocale());
         collator.setStrength(Collator.PRIMARY);
-        Collections.sort(beans, new Comparator<B>() {
-            @Override
-            public int compare(B bean1, B bean2) {
-                return collator.compare(bean1.getDisplayName(dbBeanLanguage), bean2.getDisplayName(dbBeanLanguage));
-            }
-        });
+        beans.sort((bean1, bean2) ->
+                collator.compare(bean1.getDisplayName(dbBeanLanguage), bean2.getDisplayName(dbBeanLanguage)));
     }
 
     public static <B extends DbBeanMultilingual> List<String> getListFromMultilingualBeans(
             Collection<B> beans,
             DbBeanLanguage dbBeanLanguage)
     {
-        List<String> names = new ArrayList<String>();
+        var names = new ArrayList<String>();
 
         for (B bean: beans)
             names.add(bean.getLabel(dbBeanLanguage));
