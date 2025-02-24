78b0da06b19abb1836b7eb2f5173235863671890
chris <git@pythoud.net>
2025-02-24 10:37:12 -0500

    Add support for dynamic HtmlFormHelper instantiation
    Introduce a new parameter to specify the HTML helper class name dynamically. Added a method to retrieve and instantiate the specified HtmlFormHelper. This improves flexibility in configuring and using HTML form helper classes.

--- beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/util/ApplicationParameters.java
+++ beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/util/ApplicationParameters.java
@@ -1,5 +1,9 @@
 package org.beanmaker.v2.runtime.util;
 
+import org.beanmaker.v2.runtime.HtmlFormHelper;
+import org.beanmaker.v2.util.Types;
+
+import javax.ws.rs.PUT;
 import java.util.Map;
 
 import java.util.concurrent.ConcurrentHashMap;
@@ -7,6 +11,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public enum ApplicationParameters {
     INSTANCE;
+
+    public static final String HTML_HELPER_CLASS_NAME_PARAMETER = "htmlHelperClassName";
 
     private final ConcurrentHashMap<String, Object> parameters = new ConcurrentHashMap<>();
     private final AtomicBoolean initialized = new AtomicBoolean(false);
@@ -38,6 +44,15 @@ public enum ApplicationParameters {
         if (!initialized.get()) {
             throw new IllegalStateException("ApplicationParameters not initialized");
         }
+    }
+
+    public HtmlFormHelper getHtmlFormHelper() {
+        ensureInitialized();
+        String className = getAsString(HTML_HELPER_CLASS_NAME_PARAMETER);
+        if (className == null)
+            throw new IllegalStateException("No HTML helper class name specified");
+
+        return Types.createInstanceOf(className, HtmlFormHelper.class);
     }
 
 }
