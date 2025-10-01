9a56aac9957a5b0a36bef8f8cfbb16c58f47ae12
chris <git@pythoud.net>
2025-10-01 15:06:01 -0500

    feat: enhance `FormatCheckHelper` with new parsing methods and deprecations
    Introduce overloaded `isNumber` methods supporting `DecimalValueParser` with optional positive-only validation. Add enhancements like `isIntNumber` and `isLongNumber` refactor. Mark existing methods `isNumber`, `isValidIsoDate`, and `isEmailValid` as deprecated for eventual removal.

--- beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/FormatCheckHelper.java
+++ beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/FormatCheckHelper.java
@@ -1,44 +1,66 @@
 package org.beanmaker.v2.runtime;
 
+import org.beanmaker.v2.util.DecimalValueParser;
 import org.beanmaker.v2.util.EmailValidator;
 import org.beanmaker.v2.util.SimpleInputDateFormat;
 
 import java.util.regex.Pattern;
 
 public class FormatCheckHelper {
-	
-	public static boolean isNumber(final String s) {
+
+    private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
+    private static final SimpleInputDateFormat ISO_DATE_FORMAT =
+            new SimpleInputDateFormat(SimpleInputDateFormat.ElementOrder.YYMD, "-");
+
+    @Deprecated
+    public static boolean isNumber(String s) {
 		return NUMBER_PATTERN.matcher(s).matches();
 	}
-	
-	public static boolean isValidIsoDate(final String s) {
+
+    @Deprecated
+	public static boolean isValidIsoDate(String s) {
 		return ISO_DATE_FORMAT.validate(s);
 	}
-	
-	public static boolean isEmailValid(final String s) {
+
+    @Deprecated
+	public static boolean isEmailValid(String s) {
 		return EmailValidator.validate(s, true, true);
 	}
 	
-	private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
-	private static final SimpleInputDateFormat ISO_DATE_FORMAT = new SimpleInputDateFormat(SimpleInputDateFormat.ElementOrder.YYMD, "-");
-
-	public static boolean isIntNumber(final String s) {
+	public static boolean isIntNumber(String number) {
 		try {
-			Integer.parseInt(s);
-		} catch (final NumberFormatException nfex) {
+			Integer.parseInt(number);
+		} catch (NumberFormatException nfex) {
 			return false;
 		}
 
 		return true;
 	}
 
-	public static boolean isLongNumber(final String s) {
+	public static boolean isLongNumber(String number) {
 		try {
-			Long.parseLong(s);
-		} catch (final NumberFormatException nfex) {
+			Long.parseLong(number);
+		} catch (NumberFormatException nfex) {
 			return false;
 		}
 
 		return true;
 	}
+
+    public static boolean isNumber(String number, DecimalValueParser parser) {
+        return isNumber(number, parser, false);
+    }
+
+    public static boolean isNumber(String number, DecimalValueParser parser, boolean positiveOnly) {
+        try {
+            var value = parser.parse(number);
+            if (positiveOnly && value.isNegative())
+                return false;
+        } catch (NumberFormatException nfex) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
