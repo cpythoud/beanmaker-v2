aee1b4a475a3c929b69462d244ff32a37ef8c1d7
chris <git@pythoud.net>
2025-11-11 17:56:23 -0500

    feat: add `MemorySizes` utility class for common memory unit constants
    Introduce `MemorySizes` class in `Beanmaker2` with constants for KB, MB, GB, and TB to standardize memory size calculations across the codebase.

---
+++ beanmaker2-util/src/main/java/org/beanmaker/v2/util/MemorySizes.java
@@ -0,0 +1,10 @@
+package org.beanmaker.v2.util;
+
+public class MemorySizes {
+
+    public static final long KB = 1024;
+    public static final long MB = KB * KB;
+    public static final long GB = KB * MB;
+    public static final long TB = KB * GB;
+
+}
