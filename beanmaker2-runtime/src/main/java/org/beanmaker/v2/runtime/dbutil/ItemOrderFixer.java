b5202f8848331ace1c2e0dbf016ea3b578b43284
chris <git@pythoud.net>
2026-01-06 17:46:44 -0500

    feat: add `ItemOrderFixer` utility for managing and fixing item order in database tables
    Introduce `ItemOrderFixer` class to handle item order corrections in database tables. Supports ordering based on primary or secondary fields. Provides methods to collect IDs, correct ordering, and commit changes as a transaction. Enhances modularity and simplifies reuse in item order management workflows.

---
+++ beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/dbutil/ItemOrderFixer.java
@@ -0,0 +1,100 @@
+package org.beanmaker.v2.runtime.dbutil;
+
+import org.dbbeans.sql.DBTransaction;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ItemOrderFixer {
+
+    private final DBTransaction transaction;
+    private final String tableName;
+    private final String secondaryField;
+
+    private final List<List<Long>> idLists = new ArrayList<>();
+
+    public ItemOrderFixer(DBTransaction transaction, String tableName) {
+        this(transaction, tableName, null);
+    }
+
+    public ItemOrderFixer(DBTransaction transaction, String tableName, String secondaryField) {
+        this.transaction = transaction;
+        this.tableName = tableName;
+        this.secondaryField = secondaryField;
+
+        collectIds();
+    }
+
+    private void collectIds() {
+        if (secondaryField == null) {
+            idLists.add(getAllIdsInOrder());
+            return;
+        }
+
+        var secondaryIds = getSecondaryIds();
+        for (long id: secondaryIds)
+            idLists.add(getIdsForSecondaryIdInOrder(id));
+    }
+
+    private List<Long> getAllIdsInOrder() {
+        return transaction.addQuery(
+                "SELECT id FROM " + tableName + " ORDER BY item_order",
+                ListOf::longs
+        );
+    }
+
+    private Set<Long> getSecondaryIds() {
+        var secondaryIds = new HashSet<Long>();
+        transaction.addQuery(
+                "SELECT DISTINCT " + secondaryField + " FROM " + tableName,
+                rs -> {
+                    while (rs.next()) {
+                        long id = rs.getLong(1);
+                        if (rs.wasNull())
+                            secondaryIds.add(0L);
+                        else
+                            secondaryIds.add(id);
+                    }
+                }
+        );
+        return secondaryIds;
+    }
+
+    private List<Long> getIdsForSecondaryIdInOrder(long id) {
+        String query = id == 0L ?
+                "SELECT id FROM " + tableName + " WHERE " + secondaryField + " IS NULL ORDER BY item_order" :
+                "SELECT id FROM " + tableName + " WHERE " + secondaryField + " = ? ORDER BY item_order";
+
+        return transaction.addQuery(
+                query,
+                stat -> {
+                    if (id > 0)
+                        stat.setLong(1, id);
+                },
+                ListOf::longs
+        );
+    }
+
+    public void fixOrder() {
+        for (var idList: idLists)
+            fixOrder(idList);
+
+        transaction.commit();
+    }
+
+    private void fixOrder(List<Long> idList) {
+        long[] itemOrder = { 0 };
+        for (long id: idList) {
+            transaction.addUpdate(
+                    "UPDATE " + tableName + " SET item_order = ? WHERE id = ?",
+                    stat -> {
+                        stat.setLong(1, ++itemOrder[0]);
+                        stat.setLong(2, id);
+                    }
+            );
+        }
+    }
+
+}
