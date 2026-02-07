175c352330701ef3b5638934bb67cbd4b7530d83
chris <git@pythoud.net>
2026-02-07 17:12:08 -0500

    feat: add `OnLabelSorter` utility for sorting beans by localized labels
    Introduce `OnLabelSorter` class in the runtime utility package to enable sorting of beans based on localized labels. Implements support for custom collators using locale-specific sorting.

---
+++ beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/util/OnLabelSorter.java
@@ -0,0 +1,59 @@
+package org.beanmaker.v2.runtime.util;
+
+import org.beanmaker.v2.runtime.DbBeanInterface;
+import org.beanmaker.v2.runtime.DbBeanLanguage;
+import org.beanmaker.v2.runtime.DbBeanLocalization;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+public class OnLabelSorter<B extends DbBeanInterface> {
+
+    private final List<Item> items = new ArrayList<>();
+
+    private class Item {
+        private final String label;
+        private final B bean;
+
+        private Item(String label, B bean) {
+            this.label = label;
+            this.bean = bean;
+        }
+    }
+
+    public void clear() {
+        items.clear();
+    }
+
+    public void add(String label, B bean) {
+        items.add(new Item(label, bean));
+    }
+
+    public List<B> getSortedBeans(DbBeanLocalization localization) {
+        return getSortedBeans(localization.getLanguage());
+    }
+
+    public List<B> getSortedBeans(DbBeanLanguage language) {
+        return getSortedBeans(language.getLocale());
+    }
+
+    public List<B> getSortedBeans(Locale locale) {
+        var collator = Collator.getInstance(locale != null ? locale : Locale.getDefault());
+
+        var sortedItems = new ArrayList<>(items);
+        sortedItems.sort(Comparator.comparing(
+                item -> item.label,
+                Comparator.nullsFirst(collator)
+        ));
+
+        var beans = new ArrayList<B>(sortedItems.size());
+        for (var item : sortedItems) {
+            beans.add(item.bean);
+        }
+        return beans;
+    }
+
+}
