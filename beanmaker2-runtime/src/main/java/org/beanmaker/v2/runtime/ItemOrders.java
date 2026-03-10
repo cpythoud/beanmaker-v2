144b274a296facb1bfc3f52631fded4b8df96fd9
chris <git@pythoud.net>
2026-03-10 11:53:17 -0500

    feat: add sorting for beans by item order when the order is not associated to a secondary field
    - Introduced `globalSort` method in `ItemOrders` to sort beans by their `itemOrder` values.
    - Added validation to reject sorting beans linked to a secondary field using `isItemOrderLinkedToSecondaryField`.
    - Enhances utility for handling item order sorting across collections.

--- beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/ItemOrders.java
+++ beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/ItemOrders.java
@@ -1,7 +1,9 @@
 package org.beanmaker.v2.runtime;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ItemOrders {
@@ -48,4 +50,13 @@ public class ItemOrders {
     {
         return getRange(firstItem.getItemOrder(), lastItem.getItemOrder(), itemInventory);
     }
+
+    public static <B extends DbBeanWithItemOrder> List<B> globalSort(Collection<B> beans) {
+        var sortedBeans = new ArrayList<>(beans);
+        if (!sortedBeans.isEmpty() && sortedBeans.get(0).isItemOrderLinkedToSecondaryField())
+            throw new IllegalArgumentException("Beans with a secondary field cannot be sorted by this function");
+        sortedBeans.sort(Comparator.comparingLong(DbBeanWithItemOrder::getItemOrder));
+        return sortedBeans;
+    }
+
 }
