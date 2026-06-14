ed42f2a0bec6ed023162873d41e8849d5e22d6fb
chris <git@pythoud.net>
2026-06-14 09:22:05 -0500

    fix(runtime): inline `SecureQuery` builders in `SidManager` methods to avoid potential concurrency problems
    - Replaced static `SecureQuery.Builder` fields with inline builder instances for `createMissingSids` and `resetAllSids`.

--- beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/dbutil/SidManager.java
+++ beanmaker2-runtime/src/main/java/org/beanmaker/v2/runtime/dbutil/SidManager.java
@@ -19,14 +19,9 @@ public final class SidManager {
         return token;
     }
 
-    private static final SecureQuery.Builder EXISTING_SIDS_QUERY =
-            SecureQuery.builder("SELECT sid FROM [TABLE_1] WHERE sid IS NOT NULL");
-    private static final SecureQuery.Builder ADD_SID_UPDATE =
-            SecureQuery.builder("UPDATE [TABLE_1] SET sid=? WHERE sid IS NULL");
-
     public static void createMissingSids(DbTransaction transaction, String table) {
         var existingSids = transaction.processQuery(
-                EXISTING_SIDS_QUERY.table(1, table).build(),
+                SecureQuery.builder("SELECT sid FROM [TABLE_1] WHERE sid IS NOT NULL").table(1, table).build(),
                 rs -> {
                     var sids = new HashSet<String>();
                     while (rs.next())
@@ -35,7 +30,7 @@ public final class SidManager {
                 }
         );
         transaction.processUpdate(
-                ADD_SID_UPDATE.table(1, table).build(),
+                SecureQuery.builder("UPDATE [TABLE_1] SET sid=? WHERE sid IS NULL").table(1, table).build(),
                 stat -> {
                     String sid = createSid();
                     while (existingSids.contains(sid))
@@ -46,12 +41,10 @@ public final class SidManager {
         );
     }
 
-    private static final SecureQuery.Builder RESET_SID_UPDATE = SecureQuery.builder("UPDATE [TABLE_1] SET sid=?");
-
     public static void resetAllSids(DbTransaction transaction, String table) {
         var newSids = new HashSet<String>();
         transaction.processUpdate(
-                RESET_SID_UPDATE.table(1, table).build(),
+                SecureQuery.builder("UPDATE [TABLE_1] SET sid=?").table(1, table).build(),
                 stat -> {
                     String sid = createSid();
                     while (newSids.contains(sid))
